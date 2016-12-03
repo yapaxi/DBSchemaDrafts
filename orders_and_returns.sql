05eb3c32060c2e00501efe826170a5c4d0c29849
igor.tishkin <igor.tishkin@inn.ru>
2016-12-04 01:32:20 +0300

    tabs to spaces

--- orders_and_returns.sql
+++ orders_and_returns.sql
@@ -2,58 +2,58 @@ begin tran
 go
 create table dbo.[Order]
 (
-	Id int not null primary key,
-	VenueOrderId varchar(128) not null,
-	SellingVendorId int not null,
-	/*
-	...
-	*/
-	constraint UC_Order_OrderForVendor unique(VenueOrderId, SellingVendorId),
+    Id int not null primary key,
+    VenueOrderId varchar(128) not null,
+    SellingVendorId int not null,
+    /*
+    ...
+    */
+    constraint UC_Order_OrderForVendor unique(VenueOrderId, SellingVendorId),
 );
 
 create table dbo.[OrderLine]
 (
-	Id int not null primary key,
-	VenueOrderId varchar(128) not null,
-	SellingVendorId int not null,
-	VenueOrderLineId varchar(128) not null,
-	/*
-	...
-	*/
-	constraint UC_OrderLine_OrderLineForOrder unique(VenueOrderLineId, VenueOrderId),
-	constraint UC_OrderLine_OrderLineForVendor unique(VenueOrderLineId, SellingVendorId),	
-	constraint FK_OrderLine_Order foreign key (VenueOrderId, SellingVendorId)
-									references dbo.[Order](VenueOrderId, SellingVendorId)							
+    Id int not null primary key,
+    VenueOrderId varchar(128) not null,
+    SellingVendorId int not null,
+    VenueOrderLineId varchar(128) not null,
+    /*
+    ...
+    */
+    constraint UC_OrderLine_OrderLineForOrder unique(VenueOrderLineId, VenueOrderId),
+    constraint UC_OrderLine_OrderLineForVendor unique(VenueOrderLineId, SellingVendorId),    
+    constraint FK_OrderLine_Order foreign key (VenueOrderId, SellingVendorId)
+                                  references dbo.[Order](VenueOrderId, SellingVendorId)                            
 )
 
 create table dbo.[OrderReturn]
 (
-	Id int not null primary key,
-	VenueOrderReturnId varchar(128) not null,
-	VenueOrderId varchar(128) not null,
-	SellingVendorId int not null,
-	/*
-	...
-	*/
-	constraint UC_OrderReturn_ReturnForVendor unique(VenueOrderReturnId, SellingVendorId),
-	constraint FK_OrderReturn_Order foreign key (VenueOrderId, SellingVendorId)
-									references dbo.[Order](VenueOrderId, SellingVendorId)
+    Id int not null primary key,
+    VenueOrderReturnId varchar(128) not null,
+    VenueOrderId varchar(128) not null,
+    SellingVendorId int not null,
+    /*
+    ...
+    */
+    constraint UC_OrderReturn_ReturnForVendor unique(VenueOrderReturnId, SellingVendorId),
+    constraint FK_OrderReturn_Order foreign key (VenueOrderId, SellingVendorId)
+                                    references dbo.[Order](VenueOrderId, SellingVendorId)
 )
 
 create table dbo.[OrderReturnLine]
 (
-	Id int not null primary key,
-	VenueOrderReturnId varchar(128) not null,
-	VenueOrderLineId varchar(128) not null,
-	SellingVendorId int not null,
-	/*
-	...
-	*/
-	constraint UC_OrderReturnLine_LineForReturn unique(VenueOrderLineId, VenueOrderReturnId),
-	constraint FK_OrderReturnLine_OrderReturn foreign key (VenueOrderReturnId, SellingVendorId)
-											  references dbo.[OrderReturn](VenueOrderReturnId, SellingVendorId),
-	constraint FK_OrderReturnLine_OrderLine foreign key (VenueOrderLineId, SellingVendorId)
-											  references dbo.[OrderLine](VenueOrderLineId, SellingVendorId)
+    Id int not null primary key,
+    VenueOrderReturnId varchar(128) not null,
+    VenueOrderLineId varchar(128) not null,
+    SellingVendorId int not null,
+    /*
+    ...
+    */
+    constraint UC_OrderReturnLine_LineForReturn unique(VenueOrderLineId, VenueOrderReturnId),
+    constraint FK_OrderReturnLine_OrderReturn foreign key (VenueOrderReturnId, SellingVendorId)
+                                              references dbo.[OrderReturn](VenueOrderReturnId, SellingVendorId),
+    constraint FK_OrderReturnLine_OrderLine foreign key (VenueOrderLineId, SellingVendorId)
+                                              references dbo.[OrderLine](VenueOrderLineId, SellingVendorId)
 )
 go
 rollback tran
